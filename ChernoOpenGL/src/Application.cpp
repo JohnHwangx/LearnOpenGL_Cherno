d12592ea6716277c1b4c4d81eae98ff1d6551422
JohnHwangx <johnhuangx@outlook.com>
2023-12-27 23:58:45 +0800

    多个vao的情况
    如果是一个vao，多个vbo，则vao绑定最新的vbo；
    如果有多个vao，每个vao有对应的vbo，则bindVertexarray调那个vao就画那个

--- ChernoOpenGL/src/Application.cpp
+++ ChernoOpenGL/src/Application.cpp
@@ -120,9 +120,9 @@ int main(void)
     if (!glfwInit())
         return -1;
 
-    //glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    //glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-    //glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
     /* Create a windowed mode window and its OpenGL context */
     window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
@@ -164,9 +164,9 @@ int main(void)
         2,3,0
     };
 
-    //unsigned int vao;
-    //GLCall(glGenVertexArrays(1, &vao));
-    //GLCall(glBindVertexArray(vao));
+    unsigned int vao;
+    GLCall(glGenVertexArrays(1, &vao));
+    GLCall(glBindVertexArray(vao));
 
     unsigned int buffer;
     GLCall(glGenBuffers(1, &buffer));
@@ -175,6 +175,10 @@ int main(void)
 
     GLCall(glEnableVertexAttribArray(0));
     GLCall(glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, (void*)0));
+
+    unsigned int vao2;
+    GLCall(glGenVertexArrays(1, &vao2));
+    GLCall(glBindVertexArray(vao2));
 
     unsigned int buffer2;
     GLCall(glGenBuffers(1, &buffer2));
@@ -198,7 +202,7 @@ int main(void)
     ASSERT(location != -1);
     GLCall(glUniform4f(location, 0.9f, 0.3f, 0.8f, 1.0f));
 
-    //GLCall(glBindVertexArray(0));
+    GLCall(glBindVertexArray(0));
     GLCall(glUseProgram(0));
     GLCall(glBindBuffer(GL_ARRAY_BUFFER, 0));//…Ë÷√◊¥Ã¨
     GLCall(glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0));
@@ -216,10 +220,10 @@ int main(void)
         GLCall(glUniform4f(location, r, 0.3f, 0.8f, 1.0f));
 
         //GLCall(glBindBuffer(GL_ARRAY_BUFFER, buffer));//…Ë÷√◊¥Ã¨
-        //GLCall(glBindVertexArray(vao));
-        GLCall(glBindBuffer(GL_ARRAY_BUFFER, buffer));
-        GLCall(glEnableVertexAttribArray(0));
-        GLCall(glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 3, (void*)0));
+        GLCall(glBindVertexArray(vao2));
+        //GLCall(glBindBuffer(GL_ARRAY_BUFFER, buffer));
+        //GLCall(glEnableVertexAttribArray(0));
+        //GLCall(glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 3, (void*)0));
         GLCall(glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo));
 
         GLCall(glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr));
