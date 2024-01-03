1751a1dcfd221fd44d7568197006c5eee4f80344
shanpai <shanpai@qunhemail.com>
2024-01-03 20:13:55 +0800

    shader1

---
+++ ChernoOpenGL/src/Shader.cpp
@@ -0,0 +1,106 @@
+#include "Shader.h"
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
+#include "Renderer.h"
+
+Shader::Shader(const std::string& filePath):
+	m_FilePath(filePath), m_RendererId(0)
+{
+
+}
+
+Shader::~Shader()
+{
+}
+
+ShaderProgramSource Shader::ParseShader(const std::string& filepath)
+{
+	std::ifstream stream(filepath);
+
+	enum class ShaderType
+	{
+		NONE = -1, VERTEX = 0, FRAGMENT = 1
+	};
+
+	std::string line;
+	std::stringstream ss[2];
+	ShaderType type = ShaderType::NONE;
+	while (getline(stream, line))
+	{
+		if (line.find("#shader") != std::string::npos) {
+			if (line.find("vertex") != std::string::npos)
+				type = ShaderType::VERTEX;
+			else if (line.find("fragment") != std::string::npos)
+				type = ShaderType::FRAGMENT;
+		}
+		else
+		{
+			ss[(int)type] << line << "\n";
+		}
+	}
+
+	return { ss[0].str(),ss[1].str() };
+}
+
+unsigned int Shader::CompailShader(unsigned int type, const std::string& source)
+{
+	unsigned int id;
+	GLCall(id = glCreateShader(type));
+	const char* src = source.c_str();
+	GLCall(glShaderSource(id, 1, &src, nullptr));
+	GLCall(glCompileShader(id));
+	//TODO: ERROR handing
+
+	int result;
+	GLCall(glGetShaderiv(id, GL_COMPILE_STATUS, &result));
+	if (result == GL_FALSE) {
+		int length;
+		GLCall(glGetShaderiv(id, GL_INFO_LOG_LENGTH, &length));
+		char* message = (char*)_malloca(length * sizeof(char));
+		GLCall(glGetShaderInfoLog(id, length, &length, message));
+		std::cout << "Failed to Compile " << (type == GL_VERTEX_SHADER ? "vertex" : "fragment") << " shader" << std::endl;
+		std::cout << message << std::endl;
+		GLCall(glDeleteShader(id));
+		return 0;
+	}
+
+	return id;
+	return false;
+}
+
+int Shader::CreateShader(const std::string& vertexShader, const std::string& fragmentShader)
+{
+	unsigned int program;
+	GLCall(program = glCreateProgram());
+	unsigned int vs = CompileShader(GL_VERTEX_SHADER, vertexShader);
+	unsigned int fs = CompileShader(GL_FRAGMENT_SHADER, fragmentShader);
+
+	GLCall(glAttachShader(program, vs));
+	GLCall(glAttachShader(program, fs));
+	GLCall(glLinkProgram(program));
+	GLCall(glValidateProgram(program));
+
+	GLCall(glDeleteShader(vs));
+	GLCall(glDeleteShader(fs));
+
+	return program;
+}
+
+void Shader::Bind() const
+{
+}
+
+void Shader::Unbind() const
+{
+}
+
+void Shader::SetUniform4f(std::string& name, float v0, float v1, float v2, float v3)
+{
+}
+
+unsigned int Shader::GetUniformLocation(const std::string& name)
+{
+	return 0;
+}
---
+++ ChernoOpenGL/src/Shader.h
@@ -0,0 +1,28 @@
+#pragma once
+#include <string>
+
+struct ShaderProgramSource
+{
+	std::string VertexShader;
+	std::string FragmentShader;
+};
+
+class Shader
+{
+private:
+	std::string m_FilePath;
+	unsigned int m_RendererId;
+public:
+	Shader(const std::string& filePath);
+	~Shader();
+
+	void Bind() const;
+	void Unbind() const;
+
+	void SetUniform4f(std::string& name, float v0, float v1, float v2, float v3);
+private:
+	ShaderProgramSource ParseShader(const std::string& filepath);
+	int CreateShader(const std::string& vertexShader, const std::string& fragmentShader);
+	unsigned int CompailShader(unsigned int type, const std::string& source);
+	unsigned int GetUniformLocation(const std::string& name);
+};
