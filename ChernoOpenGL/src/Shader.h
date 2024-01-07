32bc2989817afc641d0d7f475a3c65d200a8dd10
JohnHwangx <johnhuangx@outlook.com>
2024-01-07 23:06:04 +0800

    27.How to make your UNIFORMS FASTER in OpenGL

--- ChernoOpenGL/src/Shader.h
+++ ChernoOpenGL/src/Shader.h
@@ -14,7 +14,7 @@ class Shader
 private:
 	std::string m_FilePath;
 	unsigned int m_RendererId;
-	std::unordered_map<std::string, int> m_UniformLocationCache;
+	mutable std::unordered_map<std::string, int> m_UniformLocationCache;
 public:
 	Shader(const std::string& filePath);
 	~Shader();
@@ -30,5 +30,5 @@ private:
 	ShaderProgramSource ParseShader(const std::string& filepath);
 	unsigned int CreateShader(const std::string& vertexShader, const std::string& fragmentShader);
 	unsigned int CompileShader(unsigned int type, const std::string& source);
-	int GetUniformLocation(const std::string& name);
+	int GetUniformLocation(const std::string& name) const;
 };
