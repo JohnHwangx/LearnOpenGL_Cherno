a9c6080179cac84e4524f43ff980faeaf54c4851
JohnHwangx <johnhuangx@outlook.com>
2024-01-16 19:51:42 +0800

    Set Assimp

--- LearnOpenGL_Cherno/src/test/LearnOpenGL/09_LoadingModel.h
+++ LearnOpenGL_Cherno/src/test/LearnOpenGL/09_LoadingModel.h
@@ -1,1 +1,17 @@
 #pragma once
+#include "test/Test.h"
+
+namespace Test {
+	class Part3_LoadingModel :public Test
+	{
+	public:
+		Part3_LoadingModel();
+		~Part3_LoadingModel();
+
+		void OnUpdate(float deltaTime) override;
+		void OnRender() override;
+		void OnImGuiRender() override;
+	private:
+
+	};
+}
