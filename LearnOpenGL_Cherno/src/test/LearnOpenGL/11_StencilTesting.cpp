6c485e6df42f030afe87ace234840cda050c990e
JohnHwangx <johnhuangx@outlook.com>
2024-01-25 23:51:18 +0800

    Update 11_StencilTesting.cpp

--- LearnOpenGL_Cherno/src/test/LearnOpenGL/11_StencilTesting.cpp
+++ LearnOpenGL_Cherno/src/test/LearnOpenGL/11_StencilTesting.cpp
@@ -2,102 +2,11 @@
 namespace Test {
 	Part4_StencilTesting::Part4_StencilTesting()
 	{
-		//float vertices[] = {
-		//	//---- Œª÷√ ----    
-		//	-0.5f, -0.5f, -0.5f,  0.0f,  0.0f,
-		//	-0.5f,  0.5f, -0.5f,  0.0f,  1.0f,
-		//	 0.5f, -0.5f, -0.5f,  1.0f,  0.0f,
-		//	 0.5f,  0.5f, -0.5f,  1.0f,  1.0f,
-
-		//	-0.5f, -0.5f,  0.5f,  0.0f,  0.0f,
-		//	 0.5f, -0.5f,  0.5f,  0.0f,  1.0f,
-		//	-0.5f,  0.5f,  0.5f,  1.0f,  0.0f,
-		//	 0.5f,  0.5f,  0.5f,  1.0f,  1.0f,
-
-		//	-0.5f,  0.5f, -0.5f,  0.0f,  0.0f,
-		//	-0.5f,  0.5f,  0.5f,  0.0f,  1.0f,
-		//	 0.5f,  0.5f, -0.5f,  1.0f,  0.0f,
-		//	 0.5f,  0.5f,  0.5f,  1.0f,  1.0f,
-
-		//	-0.5f, -0.5f, -0.5f,  0.0f,  0.0f,
-		//	 0.5f, -0.5f, -0.5f,  0.0f,  1.0f,
-		//	-0.5f, -0.5f,  0.5f,  1.0f,  0.0f,
-		//	 0.5f, -0.5f,  0.5f,  1.0f,  1.0f,
-
-		//	-0.5f, -0.5f, -0.5f, -0.0f,  0.0f,
-		//	-0.5f, -0.5f,  0.5f, -0.0f,  1.0f,
-		//	-0.5f,  0.5f, -0.5f, -1.0f,  0.0f,
-		//	-0.5f,  0.5f,  0.5f, -1.0f,  1.0f,
-
-		//	 0.5f, -0.5f, -0.5f,  0.0f,  0.0f,
-		//	 0.5f,  0.5f, -0.5f,  0.0f,  1.0f,
-		//	 0.5f, -0.5f,  0.5f,  1.0f,  0.0f,
-		//	 0.5f,  0.5f,  0.5f,  1.0f,  1.0f,
-		//};
-
-		//unsigned int indices[] = {
-		//	// select the top left image
-		//	0, 1, 2,
-		//	1, 3, 2,
-		//	// select the top middle image
-		//	0 + 4, 1 + 4, 2 + 4,
-		//	1 + 4, 3 + 4, 2 + 4,
-		//	// select to top right image
-		//	0 + 8, 1 + 8, 2 + 8,
-		//	1 + 8, 3 + 8, 2 + 8,
-		//	// select the bottom left image
-		//	0 + 12, 1 + 12, 2 + 12,
-		//	1 + 12, 3 + 12, 2 + 12,
-		//	// select the bottom middle image
-		//	0 + 16, 1 + 16, 2 + 16,
-		//	1 + 16, 3 + 16, 2 + 16,
-		//	// select the bottom right image
-		//	0 + 20, 1 + 20, 2 + 20,
-		//	1 + 20, 3 + 20, 2 + 20,
-		//};
-
-		//float planeVertices[] = {
-		//	// positions          // texture Coords (note we set these higher than 1 (together with GL_REPEAT as texture wrapping mode). this will cause the floor texture to repeat)
-		//	 5.0f, -0.5f,  5.0f,  2.0f, 0.0f,
-		//	-5.0f, -0.5f,  5.0f,  0.0f, 0.0f,
-		//	-5.0f, -0.5f, -5.0f,  0.0f, 2.0f,
-		//	 5.0f, -0.5f, -5.0f,  2.0f, 2.0f
-		//};
-
-		//unsigned int planeIndex[] = {
-		//	0, 1, 2,
-		//	0, 2, 3,
-		//};
-
-		//m_CubeVAO = std::make_unique<VertexArray>();
-		//m_VertexBuffer = std::make_unique<VertexBuffer>(vertices, sizeof(float) * (3 + 2) * 4 * 6);
-		//VertexBufferLayout layout;
-		//layout.Push<float>(3);
-		//layout.Push<float>(2);
-		//m_CubeVAO->AddBuffer(*m_VertexBuffer, layout);
-		//m_VertexBuffer->Unbind();
-		//m_IB = std::make_unique<IndexBuffer>(indices, 36);
-
-		//m_PlaneVAO = std::make_unique<VertexArray>();
-		//m_PlaneVertexBuffer = std::make_unique<VertexBuffer>(planeVertices, sizeof(float) * (3 + 2) * 4 * 6);
-		//VertexBufferLayout planeLayout;
-		//planeLayout.Push<float>(3);
-		//planeLayout.Push<float>(2);
-		//m_PlaneVAO->AddBuffer(*m_PlaneVertexBuffer, planeLayout);
-		//m_PlaneVertexBuffer->Unbind();
-		//m_PlaneIB = std::make_unique<IndexBuffer>(planeIndex, 6);
-
 		m_Cube = std::make_unique<Cube>();
 		m_Plane = std::make_unique<Plane>();
 
 		m_Shader = std::make_unique<Shader>("res/shader/Part4_DepthTesting.shader");
 		m_StencilSingleShader = std::make_unique<Shader>("res/shader/Part4_StencilTesing_SingleColor.shader");
-
-		/*m_Textures[0] = std::make_unique<Texture>("res/textures/marble.jpg");
-		m_Textures[1] = std::make_unique<Texture>("res/textures/metal.png");*/
-
-		/*m_Shader->Bind();
-		m_Shader->SetUniform1i("texture_diffuse1", 0);*/
 
 		GLCall(glEnable(GL_DEPTH_TEST));
 		GLCall(glDepthFunc(GL_LESS));
@@ -142,11 +51,8 @@ namespace Test {
 		// draw floor as normal, but don't write the floor to the stencil buffer, we only care about the containers. We set its mask to 0x00 to not write to the stencil buffer.
 		GLCall(glStencilMask(0x00));
 		// floor
-		//m_Textures[1]->Bind();
 		m_Shader->SetUniformMat4f("u_Model", glm::mat4(1.0f));
 		m_Plane->Draw(*(m_Shader.get()));
-		/*m_PlaneVAO->Bind();
-		renderer.DrawElement(*m_PlaneVAO, *m_PlaneIB, *m_Shader);*/
 
 		// 1st. render pass, draw objects as normal, writing to the stencil buffer
 		// --------------------------------------------------------------------
@@ -154,20 +60,14 @@ namespace Test {
 		GLCall(glStencilMask(0xFF));
 
 		// cubes
-		//m_CubeVAO->Bind();
-
 		model = glm::translate(model, glm::vec3(-1.0f, 0.0f, -1.0f));
 		m_Shader->SetUniformMat4f("u_Model", model);
-
-		/*m_Textures[0]->Bind();
-		renderer.DrawElement(*m_CubeVAO, *m_IB, *m_Shader);*/
 		m_Cube->Draw(*(m_Shader.get()));
 
 		model = glm::mat4(1.0f);
 		model = glm::translate(model, glm::vec3(2.0f, 0.0f, 0.0f));
 		m_Shader->SetUniformMat4f("u_Model", model);
 		m_Cube->Draw(*(m_Shader.get()));
-		//renderer.DrawElement(*m_CubeVAO, *m_IB, *m_Shader);
 
 		// 2nd. render pass: now draw slightly scaled versions of the objects, this time disabling stencil writing.
 		// Because the stencil buffer is now filled with several 1s. The parts of the buffer that are 1 are not drawn, thus only drawing 
@@ -182,23 +82,17 @@ namespace Test {
 		float scale = 1.1f;
 
 		// cubes
-		/*m_CubeVAO->Bind();
-		m_Textures[0]->Bind();*/
 		model = glm::mat4(1.0f);
 		model = glm::translate(model, glm::vec3(-1.0f, 0.0f, -1.0f));
 		model = glm::scale(model, glm::vec3(scale, scale, scale));
 		m_StencilSingleShader->SetUniformMat4f("u_Model", model);
-		//renderer.DrawElement(*m_CubeVAO, *m_IB, *m_StencilSingleShader);
 		m_Cube->Draw(*(m_StencilSingleShader.get()));
 
 		model = glm::mat4(1.0f);
 		model = glm::translate(model, glm::vec3(2.0f, 0.0f, 0.0f));
 		model = glm::scale(model, glm::vec3(scale, scale, scale));
 		m_StencilSingleShader->SetUniformMat4f("u_Model", model);
-		//renderer.DrawElement(*m_CubeVAO, *m_IB, *m_StencilSingleShader);
 		m_Cube->Draw(*(m_StencilSingleShader.get()));
-
-		//m_CubeVAO->Unbind();
 
 		GLCall(glStencilMask(0xFF));
 		GLCall(glStencilFunc(GL_ALWAYS, 0, 0xFF));
