d65fd61fdde764829939e62cfeff86a02a96acb1
JohnHwangx <johnhuangx@outlook.com>
2024-01-25 23:50:33 +0800

    模板测试使用plane和cube封装

--- LearnOpenGL_Cherno/src/test/LearnOpenGL/11_StencilTesting.h
+++ LearnOpenGL_Cherno/src/test/LearnOpenGL/11_StencilTesting.h
@@ -5,6 +5,8 @@
 #include "VertexBufferLayout.h"
 #include "Camera.h"
 #include "Texture.h"
+#include "Cube.h"
+#include "Plane.h"
 
 namespace Test {
 	class Part4_StencilTesting : public CameraTest
@@ -18,15 +20,17 @@ namespace Test {
 		void OnImGuiRender() override;
 
 	private:
-		std::unique_ptr<VertexArray> m_CubeVAO;
+		std::unique_ptr<Cube> m_Cube;
+		std::unique_ptr<Plane> m_Plane;
+		/*std::unique_ptr<VertexArray> m_CubeVAO;
 		std::unique_ptr<VertexArray> m_PlaneVAO;
 		std::unique_ptr<IndexBuffer> m_IB;
 		std::unique_ptr<IndexBuffer> m_PlaneIB;
 		std::unique_ptr<VertexBuffer> m_VertexBuffer;
 		std::unique_ptr<VertexBuffer> m_PlaneVertexBuffer;
-		std::unique_ptr<IndexBuffer> m_IndexBUffer;
+		std::unique_ptr<IndexBuffer> m_IndexBUffer;*/
 		std::unique_ptr<Shader> m_Shader;
 		std::unique_ptr<Shader> m_StencilSingleShader;
-		std::unique_ptr<Texture> m_Textures[2];
+		//std::unique_ptr<Texture> m_Textures[2];
 	};
 }
