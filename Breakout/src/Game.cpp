2cb1fede2d5cc9d8256e6e750a8a28e63a8cb0b2
JohnHwangx <johnhuangx@outlook.com>
2024-02-05 18:52:37 +0800

    Breakout Setup

---
+++ Breakout/src/Game.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include "Game.h"
+
+namespace Breakout {
+
+	Game::Game(unsigned int width, unsigned int height)
+		: State(GAME_ACTIVE), Keys(), Width(width), Height(height)
+	{
+
+	}
+
+	Game::~Game()
+	{
+
+	}
+
+	void Game::Init()
+	{
+
+	}
+
+	void Game::Update(float dt)
+	{
+
+	}
+
+
+	void Game::ProcessInput(float dt)
+	{
+
+	}
+
+	void Game::Render()
+	{
+
+	}
+}
