b9d00b0b1a65a4e6b03f9da7728f10ef3d422689
JohnHwangx <johnhuangx@outlook.com>
2024-02-05 19:19:33 +0800

    game

--- Breakout/src/Game.cpp
+++ Breakout/src/Game.cpp
@@ -1,12 +1,28 @@
 #include <iostream>
 #include "Game.h"
+#include "ResourceManager.h"
+#include "SpriteRenderer.h"
 
 namespace Breakout {
+
+	// Game-related State data
+	SpriteRenderer* Renderer;
 
 	Game::Game(unsigned int width, unsigned int height)
 		: State(GAME_ACTIVE), Keys(), Width(width), Height(height)
 	{
+		// 加载着色器
+		ResourceManager::LoadShader("shaders/sprite.vs", "shaders/sprite.frag", nullptr, "sprite");
+		// 配置着色器
+		glm::mat4 projection = glm::ortho(0.0f, static_cast<GLfloat>(this->Width),
+			static_cast<GLfloat>(this->Height), 0.0f, -1.0f, 1.0f);
 
+		ResourceManager::GetShader("sprite").Use().SetInteger("image", 0);
+		ResourceManager::GetShader("sprite").SetMatrix4("projection", projection);
+		// 设置专用于渲染的控制
+		Renderer = new SpriteRenderer(ResourceManager::GetShader("sprite"));
+		// 加载纹理
+		ResourceManager::LoadTexture("textures/awesomeface.png", GL_TRUE, "face");
 	}
 
 	Game::~Game()
